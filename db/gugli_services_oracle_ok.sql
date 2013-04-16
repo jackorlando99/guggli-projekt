4c417dbce9c1138345496c9f21b398fedf3c0dc2
Varjasi Róbert <jackorlando99@gmail.com>
2013-04-16 11:33:15 +0200

    db szkript angolositasa,es bugfixek,uj tablak

--- db/gugli_services_oracle_ok.sql
+++ db/gugli_services_oracle_ok.sql
@@ -4,158 +4,185 @@ ALTER SESSION SET NLS_DATE_LANGUAGE = HUNGARIAN;
 ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MON-DD';
 
 
-Create table userek(
-userid_auto int,
-uname varchar2(15) not null,
+Create table users(
+user_id_auto int,
+user_name varchar2(15) not null,
+first_name varchar2(50) not null,
+last_name varchar2(30) not null,
 email varchar2(255) not null,
 pwhash varchar2(256) not null,
-constraint userek_prikey primary  key (userid_auto)
+active char(1) check (active in ( 'Y', 'N' )),
+activation_code varchar2(255) not null,
+constraint users_prikey primary  key (user_id_auto)
 );
 
 
-create table csoportok(
-gid_auto int,
-gname varchar2(255) not null,
-constraint csop_prikey primary key (gid_auto)
+create table groups_(
+group_id_auto int,
+group_name varchar2(255) not null,
+constraint groups_prikey primary key (group_id_auto)
 );
 
 
-create table csoportjai(
-gid_auto int,
-userid_auto int,
-constraint gid_foreign foreign key (gid_auto) references csoportok (gid_auto),
-constraint userid_auto_foreign foreign key (userid_auto) references userek (userid_auto)
+create table user_has_group(
+id_auto int not null,
+user_id int not null,
+group_id int not null,
+constraint gid_foreign foreign key (group_id) references groups_ (group_id_auto),
+constraint userid_foreign foreign key (user_id) references users (user_id_auto),
+constraint id_prikey primary key(id_auto)
 );
 
 
-create table dokumentumok(
+create table docs(
 doc_id_auto int,
 created_at date not null,
 updated_at date not null,
 doc_name varchar2(255) not null,
-content nclob not null,
-userid int,
+content_path varchar2(255) not null,
+user_id int not null,
+display_in_calendar char(1) check (display_in_calendar in ( 'Y', 'N' )),
 constraint doc_id_auto_prikey primary key(doc_id_auto),
-constraint doc_foreign foreign key(userid) references userek(userid_auto)
+constraint user_id_foreign foreign key(user_id) references users(user_id_auto)
 );
 
-create table konyvtar(
+create table directories(
 dir_id_auto int not null,
 dir_name varchar2(255) not null,
 parent_dir int not null,
 constraint dir_id_auto_prikey primary key(dir_id_auto)
 );
 
-create table konyvtarja(
+create table doc_has_dir(
 has_dir_id_auto int not null,
 dir_id int not null,
 doc_id int not null,
 constraint has_dir_id_auto_prikey primary key(has_dir_id_auto),
-constraint has_dir_id_foreign foreign key(dir_id) references konyvtar(dir_id_auto),
-constraint has_doc_id_foreign foreign key(doc_id) references dokumentumok(doc_id_auto)
+constraint has_dir_id_foreign foreign key(dir_id) references directories(dir_id_auto),
+constraint has_doc_id_foreign foreign key(doc_id) references docs(doc_id_auto)
 );
 
-create table kapcs_konyvt_esem_csop(
-kapcs_id_auto int not null,
-tipus varchar2(50) not null,
-tipus_id int not null,
-gid int not null,
-constraint kapcs_id_auto_prikey primary key(kapcs_id_auto)
-);
-
-
-create table nevek(
-nev_id_auto int not null, 
-nev varchar2(100) not null,
-constraint nev_id_auto_prikey primary key(nev_id_auto)
-);
-
-create table nevnapok(
-nevn_id_auto int not null,
-nev_id int not null,
-honap int not null,
-nap int not null,
-fo_nap char(1) check (fo_nap in ( 'Y', 'N' )),
-constraint nevn_id_auto_prikey primary key(nevn_id_auto),
-constraint nev_id_foreign foreign key(nev_id) references nevek(nev_id_auto)
-);
-
-
-create table esemenyek(
+create table events_(
 event_id_auto int,
 event_name varchar2(100),
-userid int,
-idopont date not null,
-description varchar(255),
-nyilvanos char(1) check (fo_nap in ( 'Y', 'N' )),
-constraint  userid_foreign foreign key(userid) references userek(userid_auto),
+user_id int,
+starts_at date not null,
+ends_at date not null,
+description varchar2(255),
+public_ char(1) check (public_ in ( 'Y', 'N' )),
+constraint  events_user_id_foreign foreign key(user_id) references users(user_id_auto),
 constraint event_id_prikey primary key(event_id_auto)
 );
 
-create table cellak(
-cella_id_auto int,
-betukod varchar2 (2),
-constraint cella_id_auto_prikey primary key(cella_id_auto)
+create table group_has_dir(
+id_auto int not null,
+group_id int not null,
+dir_id not null,
+constraint group_has_dir_prikey primary key(id_auto),
+constraint group_has_dir_foreign foreign key(group_id) references groups_(group_id_auto),
+constraint dir_id_foreign foreign key(dir_id) references directories(dir_id_auto)
 );
 
-create table terkepek(
+create table group_has_event(
+id_auto int not null,
+group_id int not null,
+event_id not null,
+constraint group_has_event primary key(id_auto),
+constraint gid_foreign2 foreign key(group_id) references groups_(group_id_auto),
+constraint event_id_foreign foreign key(event_id) references events_(event_id_auto)
+);
+
+
+create table names(
+name_id_auto int not null, 
+name_ varchar2(100) not null,
+constraint name_id_auto_prikey primary key(name_id_auto)
+);
+
+create table namedays(
+id_auto int not null,
+name_id int not null,
+month_ int not null,
+day_ int not null,
+main_day char(1) check (main_day in ( 'Y', 'N' )),
+constraint id_auto_prikey primary key(id_auto),
+constraint namedays_foreign foreign key(name_id) references names(name_id_auto)
+);
+
+
+create table cells(
+cell_id_auto int,
+code varchar2 (2),
+constraint cell_id_auto_prikey primary key(cell_id_auto)
+);
+
+create table maps(
 map_id_auto int,
 map_name varchar2(100),
-
 constraint map_id_auto_prikey primary key (map_id_auto)
 );
 
-create table cellaja(
+create table map_has_cells(
 map_cell_id_auto int,
 map_id int,
-cella_id int,
-cella_img varchar(255),
+cell_id int,
+cell_img_path varchar2(255),
 constraint map_cell_id_auto_prikey primary key(map_cell_id_auto),
-constraint cella_id_foreign foreign key(cella_id) references cellak(cella_id_auto),
-constraint map_id_foreign foreign key(map_id) references terkepek(map_id_auto)
+constraint cell_id_foreign foreign key(cell_id) references cells(cell_id_auto),
+constraint map_id_foreign foreign key(map_id) references maps(map_id_auto)
 );
 
-create table helyek(
-hely_id_auto int not null,
-hely_neve varchar2(100) not null,
-hely_cime varchar2(150) not null,
-hely_tel varchar2(100),
-hely_foto_id int,
-hely_tipusa varchar2(100) not null,
-komment varchar2(255),
-hely_x int not null,
-hely_y int not null,
+
+create table places(
+place_id_auto int not null,
+name_ varchar2(100) not null,
+address varchar2(150) not null,
+phone varchar2(100),
+place_type varchar2(100) not null,
+comment_ varchar2(255),
+coord_x int not null,
+coord_y int not null,
 map_cell_id int not null,
-constraint hely_id_auto_prikey primary key(hely_id_auto),
-constraint map_cell_id_foreign foreign key(map_cell_id) references cellaja(map_cell_id_auto)
+constraint places_id_auto_prikey primary key(place_id_auto),
+constraint map_cell_id_foreign foreign key(map_cell_id) references map_has_cells(map_cell_id_auto)
 );
 
-create table esem_helyszin(
-esem_helysz_id_auto int not null,
-esem_id int not null,
-hely_id int not null,
-constraint esem_id_foreign foreign key(esem_id) references esemenyek(event_id_auto),
-constraint hely_id_foreign foreign key(hely_id) references helyek(hely_id_auto),
-constraint esem_helysz_id_auto_prikey primary key(esem_helysz_id_auto)
+create table event_has_place(
+id_auto int not null,
+event_id int not null,
+place_id int not null,
+constraint ehp_event_id_foreign foreign key(id_auto) references events_(event_id_auto),
+constraint ehp_place_id_foreign foreign key(place_id) references places(place_id_auto),
+constraint ehp__id_auto_prikey primary key(id_auto)
 );
 
-
-create table fotok(
-foto_id_auto int not null,
-hely_id int not null,
-userid int not null,
-idopont date not null,
-keszito varchar2(100),
-constraint fuserid_foreign foreign key(userid) references userek(userid_auto),
-constraint fhely_id_foreign foreign key(hely_id) references helyek(hely_id_auto),
-constraint foto_id_prikey primary key(foto_id_auto)
-);
-
-create table naptar_bejegyz(
-note_id_auto int not null,
-note varchar(255) not null,
-userid int not null,
+create table photos(
+id_auto int not null,
+place_id int not null,
+user_id int not null,
 created_at date not null,
-constraint naptar_bejegyz_prikey primary key(note_id_auto),
-constraint naptar_bejegyz_foreign foreign key(userid) references userek(userid_auto)
+owner varchar2(100),
+photo_path varchar2(255),
+constraint photos_userid_foreign foreign key(user_id) references users(user_id_auto),
+constraint photos_place_id_foreign foreign key(place_id) references places(place_id_auto),
+constraint photos_id_prikey primary key(id_auto)
+);
+
+
+create table notes(
+note_id_auto int not null,
+note varchar2(255) not null,
+user_id int not null,
+created_at date not null,
+constraint note_prikey primary key(note_id_auto),
+constraint note_user_id_foreign foreign key(user_id) references users(user_id_auto)
+);
+
+create table group_has_note(
+id_auto int not null,
+note_id int not null,
+group_id int not null,
+constraint group_has_note_prikey primary key(id_auto),
+constraint note_foreign foreign key(note_id) references notes(note_id_auto),
+constraint group_has_note_foreign foreign key(group_id) references groups_(group_id_auto)
 );
